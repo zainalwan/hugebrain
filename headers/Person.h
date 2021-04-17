569d892ec08ec7a0febcf317a5032ef96ced171b
zainalwan <zainalwan4@gmail.com>
2021-04-17 10:56:41 +0700

    Create Person struct

---
+++ headers/Person.h
@@ -0,0 +1,17 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+namespace HugeBrain
+{
+    struct Person
+    {
+        std::string name;
+        std::string phone;
+        std::string email;
+    };
+}
+
+
+#endif
