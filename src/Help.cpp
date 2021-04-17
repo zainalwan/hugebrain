9a6889bd3309e7bf627d5466859ce3a8231f213b
zainalwan <zainalwan4@gmail.com>
2021-04-18 06:45:28 +0700

    Modify help message and add id argument help message

--- src/Help.cpp
+++ src/Help.cpp
@@ -3,7 +3,7 @@
 int HugeBrain::Help::showMessage()
 {
     std::cout << "Huge Brain is a contact software. It used to store some datas about the people\n";
-    std::cout << "you know, including their name, email, and phone.\n\n";
+    std::cout << "you know, including their name, phone, and email.\n\n";
     std::cout << "Copyright 2021 by Zain Alwan Wima Irfani.\n\n";
     std::cout << "Usage: hugebrain <arguments>\n\n";
     std::cout << "List of available arguments:\n";
@@ -12,9 +12,10 @@ int HugeBrain::Help::showMessage()
     std::cout << "    --edit                  Edit a data.\n";
     std::cout << "    --delete                Delete a data.\n";
     std::cout << "    --search                Search a data.\n";
-    std::cout << "    --name=<name>           Give the name value of the data.\n";
-    std::cout << "    --phone=<phone>         Give the phone value of the data.\n";
-    std::cout << "    --email=<email>         Give the email value of the data.\n";
+    std::cout << "    --id=<id>               Give the id value when querying data.\n";
+    std::cout << "    --name=<name>           Give the name when querying data.\n";
+    std::cout << "    --phone=<phone>         Give the phone when querying data.\n";
+    std::cout << "    --email=<email>         Give the email when querying data.\n";
 
     return 0;
 }
