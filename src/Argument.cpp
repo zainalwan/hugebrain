b9c2193902dfdb132ab8aea7c2b1eb28252a170f
zainalwan <zainalwan4@gmail.com>
2021-04-20 18:32:00 +0700

    Filter argument value if there is equal symbol

--- src/Argument.cpp
+++ src/Argument.cpp
@@ -2,6 +2,9 @@
 
 std::string HugeBrain::Argument::getValue(std::string argv)
 {
-    argv = argv.substr(argv.find('=') + 1);
-    return argv;
+    if(argv.find('=') != std::string::npos)
+    {
+        return argv.substr(argv.find('=') + 1);
+    }
+    return "";
 }
